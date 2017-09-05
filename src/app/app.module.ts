7ec5b343d5293e6b0f7a73bf2eae44865099604d
yuyang <yuyang545262477@gmail.com>
2017-09-05 18:18:58 +0800

    assignment 2/3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import {FlexLayoutModule} from '@angular/flex-layout';
 import 'hammerjs';
 import {AppComponent} from './app.component';
 import {MenuComponent} from './menu/menu.component';
+import {DishdetailComponent} from './dishdetail/dishdetail.component';
 
 // noinspection JSDeprecatedSymbols
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
