2faeb9f8bfb37d9fda6234c28652e70f7493d2c8
yuyang <yuyang545262477@gmail.com>
2017-09-06 18:40:17 +0800

    basic router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,34 @@ import 'hammerjs';
 import {AppComponent} from './app.component';
 import {MenuComponent} from './menu/menu.component';
 import {DishdetailComponent} from './dishdetail/dishdetail.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {ContactComponent} from './contact/contact.component';
 
 import {DishService} from './services/dish.service';
+
+import {AppRoutingModule} from './app-routing/app-routing.module';
 
 // noinspection JSDeprecatedSymbols
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
