b82e45592c9e8921a5d1ce01753b943d8a3a1987
舒米勒 <yuyang545262477@gmail.com>
2017-09-10 19:14:34 +0800

    Reactive Forms part 3 1/2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Feedback} from '../share/feedback';
 import {ContactType} from '../share/contactType';
 
@@ -14,6 +14,35 @@ export class ContactComponent implements OnInit {
   feedback: Feedback;
   contactType = ContactType;
 
+  formErrors = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  };
+  validationMessages = {
+    'firstname': {
+      'required': 'First Name is required.',
+      'minlength': 'First Name must be at least 2 characters long.',
+      'maxlength': 'FirstName cannot be more than 25 characters long.'
+    },
+    'lastname': {
+      'required': 'Last Name is required.',
+      'minlength': 'Last Name must be at least 2 characters long.',
+      'maxlength': 'Last Name cannot be more than 25 characters long.'
+    },
+    'telnum': {
+      'required': 'Tel. number is required.',
+      'pattern': 'Tel. number must contain only numbers.'
+    },
+    'email': {
+      'required': 'Email is required.',
+      'email': 'Email not in valid format.'
+    },
+
+  };
+
+
   constructor(private _formBuilder: FormBuilder) {
     this.createForm();
   }
@@ -23,14 +52,19 @@ export class ContactComponent implements OnInit {
 
   private createForm() {
     this.feedbackForm = this._formBuilder.group({
-      firstname: '',
-      lastname: '',
-      telnum: 0,
-      email: '',
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: ['', [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+
+    this.feedbackForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged();
   }
 
   onSubmit() {
@@ -45,6 +79,10 @@ export class ContactComponent implements OnInit {
       contacttype: 'None',
       message: ''
     });
+
+  }
+
+  private onValueChanged(data?: any) {
 
   }
 }
